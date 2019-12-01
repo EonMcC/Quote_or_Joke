794b904ea5093efa3b28eef3f893c8d87aecdb9a
Iain <iain2eyes@gmail.com>
2019-12-01 11:16:15 +0000

    Three Quotes being saved

--- src/components/GameBox.vue
+++ src/components/GameBox.vue
@@ -1,10 +1,17 @@
 <template lang="html">
   <div>
     <hr>
-    <h3 v-on:click="handleClick">Roll Up, Roll Up</h3>
+    <h3>Roll Up, Roll Up</h3>
     <hr>
     <h4>Who said the following quote?</h4>
-    <p>{{realQuote.quoteText}}</p>
+    <p v-if="threeQuotes.length !== 0">{{this.threeQuotes[1].quoteText}}</p>
+    <button type="button" v-on:click="generate"></button>
+    <!-- <h6 v-if="realQuote">{{this.realQuote.quoteText}}</h6> -->
+    <!-- <div class="answer-container">
+      <p v-if="realQuote">{{this.realQuote.quoteAuthor}}</p>
+      <p v-if="fakeQuote1">{{this.fakeQuote1.quoteAuthor}}</p>
+      <p v-if="fakeQuote2">{{this.fakeQuote2.quoteAuthor}}</p>
+    </div> -->
   </div>
 </template>
 
@@ -13,32 +20,47 @@ export default {
   name: "game-box",
   data(){
     return{
+      allQuotes: [],
+      threeQuotes: [],
       realQuoteMatch: null,
       realQuote: null,
       fakeQuote1: null,
       fakeQuote2: null
     };
   },
+  mounted() {
+      fetch('https://quote-garden.herokuapp.com/quotes/all')
+      .then(res => res.json())
+      .then(quotes => this.allQuotes = quotes.results)
+  },
   methods: {
-    handleClick(){
-      fetch('https://quote-garden.herokuapp.com/quotes/random')
-      .then(res => res.json())
-      .then(quote => {
-        this.realQuoteMatch = quote;
-        this.realQuote = quote;
-      })
-      fetch('https://quote-garden.herokuapp.com/quotes/random')
-      .then(res => res.json())
-      .then(quote => this.fakeQuote1 = quote);
-
-      fetch('https://quote-garden.herokuapp.com/quotes/random')
-      .then(res => res.json())
-      .then(quote => this.fakeQuote2 = quote);
+    randomIndex(array){
+      let randomIndex = Math.floor(Math.random()* array.length);
+      return randomIndex;
+    },
+    generate: function (){
+      // this.threeQuotes = this.allQuotes[1].quoteText;
+    var i;
+    for (i = 0; i < 3; i++) {
+      this.threeQuotes.push(this.allQuotes[this.randomIndex(this.allQuotes)]);
     }
-
+  }
   }
 }
 </script>
 
 <style lang="css" scoped>
+  .answer-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+
+  p {
+    margin-left: 10px;
+    margin-right: 10px;
+    border: solid 2px black;
+    padding: 5px;
+  }
+
 </style>
