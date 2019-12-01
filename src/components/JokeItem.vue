a81c2a2836a22051f7c74d6131a9d51c4e04fb16
Iain <iain2eyes@gmail.com>
2019-12-01 08:04:18 +0000

    Reveal punchline added

--- src/components/JokeItem.vue
+++ src/components/JokeItem.vue
@@ -1,7 +1,8 @@
 <template lang="html">
   <div v-if="joke">
     <p>{{this.joke.setup}}</p>
-    <p><strong>{{this.joke.punchline}}</strong></p>
+    <p v-if="displayPunchline === true"><strong>{{this.joke.punchline}}</strong></p>
+    <p class="click" v-on:click="handleClickPunchline">(click to reveal/hide punchline)</p>
     <button type="button" v-on:click="handleClickFave">Save as a Fave</button>
   </div>
 </template>
@@ -10,14 +11,31 @@
 import {eventBus} from '../main.js';
 export default {
   name: "joke-item",
+  data(){
+    return {
+      displayPunchline: false
+    };
+  },
   props: ['joke'],
   methods: {
     handleClickFave(){
       eventBus.$emit('selected-fave-joke', this.joke);
-    }
+    },
+    handleClickPunchline(){
+      this.displayPunchline = !this.displayPunchline;
   }
+}
 }
 </script>
 
 <style lang="css" scoped>
+  .click {
+    cursor: pointer;
+    font-size: 0.5em;
+  }
+
+  .reveal {
+
+  }
+
 </style>
